4117e5e34e42eef4d50011e0724e1238b3e43a2b
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-18 12:34:26 -0400

    test for dime

--- coin_changer.rb
+++ coin_changer.rb
@@ -4,6 +4,6 @@ coins = {penny:1, nickle:5,dime:10,quarter:25}
 if coin ==1
 change={penny:1, nickle:0,dime:0,quarter:0}
 elsif coin == 5
-change={penny: 0, nickle: 5, dime: 0, quarter: 0}
+change={penny:0, nickle:5, dime:0, quarter:0}
 end
 end
