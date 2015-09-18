0a9e0167ed4c4b792fa03c2f771fbe0fb5622de1
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-18 16:33:45 -0400

    Test for 99 cents

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -24,7 +24,11 @@ assert_equal({penny:3,nickel:0,dime:0,quarter:0}, coin_change_counter(3))
 end  
 
 def test_for_16_cents
-assert_equal({quarter:0, dime:1, nickel:1, penny:1},coin_change_counter(10+5+1))
+assert_equal({penny:1, nickel:1, dime:1, quarter:0},coin_change_counter(1+5+10))
+end
+
+def test_for_99_cents
+assert_equal({penny:4, nickel:0, dime:2, quarter:3},coin_change_counter(1+1+1+1+5+5+10+25+25+25))
 end
 
 end
