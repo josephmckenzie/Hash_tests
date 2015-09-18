14d97a315144137309519cd8b0bbddf107159148
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-18 12:39:43 -0400

    test for quater

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -4,16 +4,21 @@ require "minitest/autorun"
 class Coin_changer<Minitest::Test
 
 def test_for_penny
-assert_equal({penny: 1, nickle: 0, dime: 0, quarter: 0}, coin_change_counter(1))
+assert_equal({penny:1, nickle:0, dime:0, quarter:0}, coin_change_counter(1))
 end
 
 def test_for_nickle
-assert_equal({penny: 0,nickle: 5,dime: 0, quarter: 0}, coin_change_counter(5))
+assert_equal({penny:0,nickle:5,dime:0, quarter:0}, coin_change_counter(5))
 end
 
 def test_for_dime
-assert_equal({penny: 0,nickle: 0,dime: 10, quarter: 0}, coin_change_counter(10))
+assert_equal({penny:0,nickle:0,dime:10, quarter:0}, coin_change_counter(10))
 end
+
+def test_for_quarter
+assert_equal({penny:0,nickle:0,dime:0, quarter:25}, coin_change_counter(25))
+
 end
   
 
+end
