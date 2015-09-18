11618903fad046520c17065367a3f002a95a4b0c
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-18 12:18:06 -0400

    code added to make first test pass

---
+++ coin_changer.rb
@@ -0,0 +1,7 @@
+def coin_change_counter(coin)
+# I dont have to have coins = down below do we put it just to show what the value should equal?
+coins = {penny:1, nickle:5,dime:10,quarter:25} 
+if coin ==1
+change={penny:1, nickle:0,dime:0,quarter:0}
+end
+end
--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -4,7 +4,7 @@ require "minitest/autorun"
 class Coin_changer<Minitest::Test
 
 def test_for_coins
-assert_equal({penny: 1, nickle: 0, dime: 0, quarter: 0}, coin_changer(1))
+assert_equal({penny: 1, nickle: 0, dime: 0, quarter: 0}, coin_change_counter(1))
 end
 end
   
