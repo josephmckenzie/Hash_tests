f43715ce383ba31131db7c2d4ef9ea1495c07986
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-18 12:21:54 -0400

    test for nickle

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -3,9 +3,14 @@ require "minitest/autorun"
 
 class Coin_changer<Minitest::Test
 
-def test_for_coins
+def test_for_penny
 assert_equal({penny: 1, nickle: 0, dime: 0, quarter: 0}, coin_change_counter(1))
 end
+
+def test_for_nickle
+assert_equal({penny: 0,nickle: 5,dime: 0, quarter: 0}, coin_change_counter(5))
+end
+
 end
   
 
