182aeb66f6520825b0ab047483dcb4f467aaa606
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-18 12:24:36 -0400

    added code for nickle test to work

--- coin_changer.rb
+++ coin_changer.rb
@@ -3,5 +3,7 @@ def coin_change_counter(coin)
 coins = {penny:1, nickle:5,dime:10,quarter:25} 
 if coin ==1
 change={penny:1, nickle:0,dime:0,quarter:0}
+elsif coin == 5
+change={penny: 0, nickle: 5, dime: 0, quarter: 0}
 end
 end
